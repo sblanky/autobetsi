d1fa903e62fabf45e372631b6debdda543cf1a08
L. Scott Blankenship <pcxtsbl@localhost.localdomain>
2023-01-19 16:22:12 +0000

    move cleaning to separate module

---
+++ autobetsi/cleaning.py
@@ -0,0 +1,42 @@
+from os.path import exists
+from os import mkdir
+import pandas as pd
+import glob
+
+
+def strictly_increasing(List):
+    increasing = [x < y for x, y in zip(List, List[1:])]
+    increasing.append(increasing[-1])
+    return increasing
+
+
+def clean_isotherms(
+    input_dir: str = './csv/',
+    output_dir: str = './csv_clean/'
+):
+    print(f'Cleaning isotherms in {input_dir} and placing in {output_dir}...')
+    if not exists(output_dir):
+        mkdir(output_dir)
+
+    files = glob.glob(f'{input_dir}*.csv')
+    for f in files:
+        isotherm = pd.read_csv(
+            f,
+            names=['relative_pressure', 'loading'],
+            header=0,
+        )
+
+        relative_pressure = isotherm['relative_pressure'].to_list()
+        increasing = strictly_increasing(relative_pressure)
+        isotherm = isotherm.assign(increasing=increasing)
+        isotherm = isotherm[isotherm.increasing == True]
+        isotherm = isotherm.drop(columns=['increasing'])
+
+        isotherm.to_csv(
+            f'{output_dir}{f.split(input_dir)[1]}',
+            index=False,
+        )
+
+    print(f'...{len(files)} cleaned and saved in {output_dir}')
+
+    return output_dir
