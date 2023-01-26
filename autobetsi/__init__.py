49660cbf97f9f164d2cf6e2f343c3580ef70b35a
L. Scott Blankenship <pcxtsbl@192.168.1.107>
2023-01-26 18:59:43 +0000

    small modification to setup

---
+++ autobetsi/__init__.py
@@ -0,0 +1,6 @@
+"""
+autobetsi
+"""
+
+__version__ = "0.1.0"
+__author__ = 'Stephen Hudson'
--- setup.py
+++ setup.py
@@ -1,11 +1,27 @@
 from setuptools import setup, find_packages
+import codecs
+import os.path
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
 
 setup(
     name='autobetsi',
-    version='0.1',
+    version=get_version('autobetsi/__init__.py'),
     description='Automaticly apply BETSI criteria to isotherms in a directory',
     url='https://github.com/sblanky/autobetsi',
     install_requires=[],
+    author='L. Scott Blankenship',
     author_email='leo.blankenship1@nottingham.ac.uk',
     packages=find_packages(),
     zip_safe=False,
